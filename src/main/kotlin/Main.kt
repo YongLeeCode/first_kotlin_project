094ffe3c83fd6bd98217edb8be9c2494f018fec3
Yong <yonghwan311@gmail.com>
2022-09-17 09:43:39 -0600

    git commit test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    //print은 println
+    println("What is your name?")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // input은 readln
+    val name = readln()
+
+    println("Hello, $name")
+
 }
