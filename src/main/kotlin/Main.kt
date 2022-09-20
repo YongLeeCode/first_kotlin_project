ed00628b678dbee5d640f364f95be8dc61aa6430
Yong <yonghwan311@gmail.com>
2022-09-20 12:56:34 -0600

    git commit test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,19 @@ fun main(args: Array<String>) {
     println("What is your name?")
 
     // input은 readln
+    // val은 read-only이면서 로컬 변수가 됩니다. java에서는 final, c/c++에서는 const에 해당합니다. 초기화 이후 변할 수 없는 값을 말하게 됩니다.
     val name = readln()
 
     println("Hello, $name")
+
+    //var는 일반적인 변수에 해당합니다.
+    //값이 변할 수 있습니다.
+    var number = 10;
+
+    //함수 형태 : fun 함수명(변수명: 변수 타입): 리턴 타입 { return 값 }
+    fun sum(a: int, b: int): int{
+        return a+b;
+    }
+    fun sum(a: Int, b: Int): Int = a + b
 
 }
